95428fc25090800fee820eb2abf0d2b1cd3d1c02
Justin Boyer <boyer942@aim.com>
2012-04-23 00:39:04 -0400

    Updated with repeat tag and convertPitch function.

--- mus/compiler.js
+++ mus/compiler.js
@@ -8,7 +8,11 @@ var endTime = function (time, expr) {
 	switch(expr.tag)
 	{
 		case "note":
+		case "rest":
 			return time + expr.dur;
+			
+		case "repeat":
+			return expr.dur * expr.count;
 			
 		case "seq":
 			return endTime(endTime(time, expr.left), expr.right);
@@ -27,7 +31,16 @@ var CompileExpr = function (expr, startTime) {
 	switch(expr.tag)
 	{
 		case "note":
-			return [{ tag: 'note', pitch: expr.pitch, start: startTime, dur: expr.dur }];
+		case "rest":
+			return [{ tag: 'note', pitch: convertPitch(expr.pitch), start: startTime, dur: expr.dur }];
+			
+		case "repeat":
+			var result = [];
+			for (var i = 0; i < expr.count; i++)
+			{
+				result.push(CompileExpr(expr.section, expr.dur * i);
+			}
+			return result;
 			
 		case "seq":
 			return CompileExpr(expr.left, startTime).concat(CompileExpr(expr.right, endTime(startTime, expr.left)));
@@ -36,6 +49,26 @@ var CompileExpr = function (expr, startTime) {
 			return CompileExpr(expr.left, startTime).concat(CompileExpr(expr.right, startTime));
 	
 	}
+
+};
+
+var convertPitch = function (pitch) {
+	var letterPitch = pitch[0];
+	var octave = pitch[1];
+	
+	switch(letterPitch) 
+	{
+		case: 'c': letterPitch = 0; break;
+		case: 'd': letterPitch = 2; break;
+		case: 'e': letterPitch = 4; break;
+		case: 'f': letterPitch = 5; break;
+		case: 'g': letterPitch = 7; break;
+		case: 'a': letterPitch = 9; break;
+		case: 'b': letterPitch = 11; break;
+		default: letterPitch = 0;
+	}
+	
+	return 12 + 12 * octave + letterPitch;
 
 };
 
