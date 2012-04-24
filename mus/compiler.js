3068d6907d627d33b8c8f45f2c84dbbd80fd1d5f
Justin Boyer <boyer942@aim.com>
2012-04-23 23:22:08 -0400

    Refined repeat tag code.

--- mus/compiler.js
+++ mus/compiler.js
@@ -38,7 +38,7 @@ var CompileExpr = function (expr, startTime) {
 			var result = [];
 			for (var i = 0; i < expr.count; i++)
 			{
-				result.push(CompileExpr(expr.section, expr.dur * i));
+				result.push(CompileExpr(expr.section, (endTime(0, expr.section) * i)));
 			}
 			return result;
 			
@@ -71,4 +71,30 @@ var convertPitch = function (pitch) {
 	return 12 + 12 * octave + letterPitch;
 
 };
+
+// simple tests for functionality
+var melody_mus = {
+  tag: 'seq',
+  left: {
+    tag: 'repeat', count: 2,
+    section: {
+      tag: 'seq',
+      left: {
+        tag: 'repeat', count: 2,
+        section: {
+          tag: 'note', pitch: 'a4', dur: 250
+        }
+      },
+      right: { tag: 'note', pitch: 'b4', dur: 250 }
+    }
+  },
+  right: {
+    tag: 'seq',
+    left: { tag: 'note', pitch: 'c4', dur: 500 },
+    right: { tag: 'note', pitch: 'd4', dur: 500 }
+  }
+};
+
+console.log(melody_mus);
+console.log(compile(melody_mus));
 
